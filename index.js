f45f333394f324328c45ac4105a7cec123cbe81f
tristyn <trisclassie@gmaiil.com>
2022-08-23 20:16:24 -0600

    commit tree 3235635fb73e4d2a112e1d4fb7984b36f7425ae5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+
+   return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then(response => {
+            document.body.innerHTML = `<p>${response.id}<p>`
+    })
+    .catch(message => {
+        // console.log("error", message)
+            document.body.innerHTML = `<p> ${message}<p>`
+    })
+}
